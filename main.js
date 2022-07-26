1c9a142517d216db9cc531f9e3f02bb336c0dd4f
alejo <alejovazquez276@gmail.com>
2022-07-26 15:32:22 -0300

    agrego if else if

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ let edades
 let promedio
 let suma = 0
 
-let NEdades = parseInt(prompt("Digite cual es la cantidad de personas registradas: "))
+let numeroDeEdades = parseInt(prompt("Digite cual es la cantidad de personas registradas: "))
 
-for (i = 0; i< NEdades; i++) {
+for (i = 0; i< numeroDeEdades; i++) {
     let edades = parseInt(prompt("Digite las edades: " + i))
     suma = suma + edades
-    console.log("Las edades son: " + edades);
+    if (edades < 18) {
+        console.log("Es menor de edad: " + edades);
+    }else if (edades >= 60) {
+        console.log("Es una persona mayor: " + edades);
+    }else{
+        console.log("La edad es: " + edades);
+    }
 }
 
-promedio = suma / NEdades
+promedio = suma / numeroDeEdades
 
 alert("El promedio de las edades es: " + promedio)
 
-console.log("El promedio de las edades es: " + promedio);
+console.log("El promedio de las edades es: " + promedio); 
